a6f29fa5de1d88851aa1afdd957944ef3042db19
Disbeidy Anzueta Gongora <disbeidyanzueta@gmail.com>
2026-05-30 19:52:58 -0500

    Listar usuario

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const pool = require('../config/db');
 const router = express.Router();
 
+// Crear usuario
 router.post('/usuarios', async (req, res) => {
   const { nombre, email, password, rol } = req.body;
   if (!['admin','docente'].includes(rol)) {
@@ -14,6 +15,12 @@ router.post('/usuarios', async (req, res) => {
     [nombre, email, hashedPassword, rol]
   );
   res.json({ message: 'Usuario creado con éxito' });
+});
+
+// Listar usuarios
+router.get('/usuarios', async (req, res) => {
+  const [rows] = await pool.query('SELECT id, nombre, email, rol FROM usuarios');
+  res.json(rows);
 });
 
 module.exports = router;
